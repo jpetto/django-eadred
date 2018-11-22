5b3ffdfd8eccb55c53908198eda1598cb4461ba1
Jon Petto <jpetto@users.noreply.github.com>
2018-11-21 21:02:26 -0600

    Use Python's built-in import_module (instead of Django's)
    - Django's import_module was deprecated in 1.9

--- eadred/management/commands/generatedata.py
+++ eadred/management/commands/generatedata.py
@@ -2,8 +2,8 @@ import imp
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
-from django.utils.importlib import import_module
 
+from importlib import import_module
 from optparse import make_option
 
 
